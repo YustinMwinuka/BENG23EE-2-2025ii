6e280cac417e9a2b2d592191a2f165d32d13f323
i CEP <josephchongola43@gmail.com>
2025-04-11 15:22:16 +0300

    GROUP NUMBER 3 ASSIGNMENT

---
+++ GROUP 3 EE2  ASSIGNMENT (Student management system)/src/program.java
@@ -0,0 +1,15 @@
+public class program {
+    String depertmentName;
+    String programName;
+    String programCode;
+    int credit;
+    public void data2(){
+        //display
+        System.out.println("You have successful entered program info");
+        System.out.println("program NAME is "+ programName);
+        System.out.println("program DEPERTMENT is "+ depertmentName);
+        System.out.println("program CODE is "+ programCode);
+        System.out.println("program CREDIT is "+ credit);
+    }
+
+}
