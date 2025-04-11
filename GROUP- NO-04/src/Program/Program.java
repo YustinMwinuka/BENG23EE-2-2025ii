24aaabca21e73a66311b08e5ff2239978b6ad8c3
Nadia-2507 <nadiasalmin25@gmail.com>
2025-04-11 04:54:09 -0700

    Add files via upload

---
+++ GROUP- NO-04/src/Program/Program.java
@@ -0,0 +1,19 @@
+package Program;
+
+public class Program {
+
+    String name;
+
+    String code;
+
+    String desc;
+
+    public Program(){
+
+    }
+    public void DisplayProgram(){
+        System.out.println("Program Name:" + name);
+        System.out.println("Program Code:" + code);
+        System.out.println("Program Description:" + desc);
+    }
+}
