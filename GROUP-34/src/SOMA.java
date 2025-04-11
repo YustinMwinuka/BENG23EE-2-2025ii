2c81fcd06093b713ee19ad113aa037cce82de8fd
Anzaamen <anzaamenlema24@gmail.com>
2025-04-11 16:42:23 +0300

    GROUP NUMBWER 34 ASSIGNMENT

---
+++ GROUP-34/src/SOMA.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class SOMA {
+    public static void main(String[] args) {
+        boolean running = true;
+
+        while (running) {
+            int number;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("ENTER CHOICE:   \n1. Student Management \n2. Program Management \n3. Exit Program\n");
+            number = sc.nextInt();
+
+            switch (number) {
+                case 1:
+                    System.out.println("Enter student name: ");
+                    String name = sc.next();
+                    System.out.println("Enter Student registration number");
+                    String registrationNumber = sc.next();
+                    System.out.println("Enter student age");
+                    int age = sc.nextInt();
+                    System.out.println("Enter Student status");
+                    String status = sc.next();
+
+                    Student s = new Student(name,registrationNumber, age, status);
+                    s.displayStudent();
+                    break;
+                case 2:
+                    System.out.println("Enter program name: ");
+                    String programName = sc.next();
+                    System.out.println("Enter program code: ");
+                    String code = sc.next();
+                    System.out.println("Enter program description: ");
+                    String desc = sc.next();
+
+                    Program p = new Program(programName, code, desc);
+                    p.displayProgram();
+                    break;
+                case 3:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
+    }
+}
