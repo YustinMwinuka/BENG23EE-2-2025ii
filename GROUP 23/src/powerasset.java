6ce2e6082b94225ac13620bf3b52a3088a315bb4
pipsknight <pipsknight90@gmail.com>
2025-05-13 14:10:02 +0300

    GROUP N0 23 ASSIGNMENT 03

---
+++ GROUP 23/src/powerasset.java
@@ -0,0 +1,38 @@
+public class powerasset {
+    protected string assetID;
+    protected string name;
+    protected string type;
+    protected sting location;
+    protected sting date;
+    protected string condtion;
+    protected string maintenance;
+
+    public powerasset(string id, string name, string type, string location, string date, string, condtion, string maintenance) {
+        this.assetID = id
+        this.name = name;
+        this.type = type;
+        this.location = location;
+        this.date = date;
+        this.condition = condtion;
+        this.voltage = voltage;
+        this.maintanence = maintenance
+    }
+}
+}
+public void displaydetails(){
+    syestem.out.println("ID:"+ assetID);
+    sytem.out.println("name:" + name);
+    syestem.out.println("type: " + type);
+    system.out.println(" location" + location);
+    system.out.println(" installationdate" + installatipondate);
+    system.out.println("condtion"+ condtion);
+    system.out.println(" voaltage"+ voaltage
+    system.out.println("maintaenance"+ maintenance);
+
+    public class main{
+        public static main void(string[] args){
+            powerasset asset=new power asset("PA001","Transformer A" "Transformer" "substation "   )
+        }
+    }
+
+}
