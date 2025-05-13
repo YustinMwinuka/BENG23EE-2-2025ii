66c84104566c60e12c3e751a7eee9d8a1c226a86
Mathewmalyangomalyango <malyangomathew2000@gmail.com>
2025-05-13 07:57:33 -0700

    GROUP NUMBER 37 ASSIGNMENT

---
+++ Group 37/src/Vehicle.java
@@ -0,0 +1,10 @@
+// Parent class
+class Vehicle {
+    Vehicle() {
+        System.out.println("Vehicle constructor called.");
+    }
+
+    void start() {
+        System.out.println("Vehicle is starting...");
+    }
+}
